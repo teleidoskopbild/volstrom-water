85d34f06444953d5a0453c261e85d567aebdb54a
teleidoskopbild <teleidoskopbild@gmail.com>
2025-02-26 18:56:47 +0100

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div
+    id="app"
+    class="overflow-hidden h-screen bg-[url('images/mountains.jpg')] bg-cover bg-center"
+  >
+    <div class="mt-6 ml-6 flex flex-col gap-4">
+      <h1 class="text-5xl font-amulya">VOLSTROM</h1>
+      <p class="text-4xl font-synonym">Designer Water</p>
+    </div>
+    <img
+      src="/images/water.png"
+      alt="water-bottle"
+      class="mt-8 md:ml-16 max-h-[75vh] w-auto object-contain"
+    />
+  </div>
+</template>
+
+<style scoped></style>
